bea0fdd89b0ba2eb2db15501f708fc51751dbf5a
Clay McKee <cmckee120993@gmail.com>
2022-08-03 16:20:03 -0700

    I added an id to each row that would contain the hour for the row. I then accessed that in the JS by using the id of the row as the value. This then allowed me to check that value based on the time to show past/present/future and employ the color coding as directed in the CSS by adding a class to the row element.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,50 @@
 // Display current date and time on the jumbotron
 let timeEl = $('#currentDay');
-
+let currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
 function time() {
-    let currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeEl.text(currentTime);
 }
 
 setInterval(time, 1000);
+
+// Coloring the schedule as past, present, or future
+// Variables for the content from the rows
+// let nine = $("#time-9").textContent;
+// let ten = $("#time-10").textContent;
+// let eleven = $("#time-11").textContent;
+// let twelve = $("#time-12").textContent;
+// let one = $("#time-1").textContent;
+// let two = $("#time-2").textContent;
+// let three = $("#time-3").textContent;
+// let four = $("#time-4").textContent;
+// let five = $("#time-5").textContent;
+
+// Current hour variable
+let currentHour = moment().format('h a');
+console.log(currentHour)
+
+let rows = $('.row');
+
+Array.from(rows).forEach( row => {
+    let rowIdString = row.id;
+    
+    if (currentHour > rowIdString) {
+        row.classList.add('past');
+    } else if (currentHour === rowIdString) {
+        row.classList.add('present');
+    } else if (currentHour < rowIdString) {
+        row.classList.add('future')
+    }
+})
+// Creating an array of the rows
+// let timeArray = [nine, ten, eleven, twelve, one, two, three, four, five];
+
+// function timeByColor() {
+//     for (let i =0; i<timeArray.length; i++) {
+//         if (timeArray[i] < currentHour) {
+//         element.classList.add("past")
+//         } else if (timeArray[i] = currentHour) {
+//             element.classList.add("present")
+//             } 
+//     }
+// };
