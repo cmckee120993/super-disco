6835caf6ab9f46d0bf8bccd606d2b6a8bc121e31
Clay McKee <cmckee120993@gmail.com>
2022-08-03 17:59:31 -0700

    Added a parseInt function to the row id and I changed the functionaly of the color change to military time to assure that the colors matched the time properly. The 9 was previously always green while the others changed, adding these two to the JS fixed the problem.

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,21 @@
 // Display current date and time on the jumbotron
 let timeEl = $('#currentDay');
-let currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+let currentDate = moment().format('MMMM DD, YYYY');
 function time() {
-    timeEl.text(currentTime);
+    timeEl.text(currentDate);
 }
 
 setInterval(time, 1000);
 
-// Coloring the schedule as past, present, or future
-// Variables for the content from the rows
-// let nine = $("#time-9").textContent;
-// let ten = $("#time-10").textContent;
-// let eleven = $("#time-11").textContent;
-// let twelve = $("#time-12").textContent;
-// let one = $("#time-1").textContent;
-// let two = $("#time-2").textContent;
-// let three = $("#time-3").textContent;
-// let four = $("#time-4").textContent;
-// let five = $("#time-5").textContent;
-
 // Current hour variable
-let currentHour = moment().format('h a');
-console.log(currentHour)
+let currentHour = moment().format('H');
+console.log(currentHour);
 
 let rows = $('.row');
 
-Array.from(rows).forEach( row => {
-    let rowIdString = row.id;
+// Creating an array of rows and adding a color function
+Array.from(rows).forEach( function(row) {
+    let rowIdString = parseInt(row.id);
     
     if (currentHour > rowIdString) {
         row.classList.add('past');
@@ -36,15 +25,4 @@ Array.from(rows).forEach( row => {
         row.classList.add('future')
     }
 })
-// Creating an array of the rows
-// let timeArray = [nine, ten, eleven, twelve, one, two, three, four, five];
 
-// function timeByColor() {
-//     for (let i =0; i<timeArray.length; i++) {
-//         if (timeArray[i] < currentHour) {
-//         element.classList.add("past")
-//         } else if (timeArray[i] = currentHour) {
-//             element.classList.add("present")
-//             } 
-//     }
-// };
